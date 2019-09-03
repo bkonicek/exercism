7f53be01f87e44772a23a6c50f183e860b142781
Ben Konicek <ben.konicek@gmail.com>
2019-09-03 17:09:33 +0000

    submission for leap year exercise

---
+++ go/leap/leap.go
@@ -0,0 +1,16 @@
+package leap
+
+// IsLeapYear takes any year and returns true if it
+// is a leap year and false if not.
+func IsLeapYear(year int) bool {
+	if year%4 == 0 {
+		if year%100 == 0 {
+			if year%400 == 0 {
+				return true
+			}
+			return false
+		}
+		return true
+	}
+	return false
+}
