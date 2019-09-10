ddbdf558ee1c8142ed4aad631b0149d20093f70b
Ben Konicek <ben.konicek@gmail.com>
2019-09-10 19:51:31 +0000

    remove unnecessary extra function/iteration

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -2,31 +2,34 @@ package raindrops
 
 import "strconv"
 
-// FindFactors returns an array of all factors for a given integer
-func FindFactors(num int) []int {
-	var factors []int
-	for i := 1; i <= num; i++ {
-		if num%i == 0 {
-			factors = append(factors, i)
-		}
-	}
-	return factors
-}
+const three = "Pling"
+const five = "Plang"
+const seven = "Plong"
 
 // Convert takes an integer and returns 'Pling' if it has 3 as a factor,
 // 'Plang' if it has 5 as a factor, and 'Plong' if it has 7 as a factor.
 // If more than 1 of the above factors apply, their respective strings are all returned.
 // If neither 3, 5, or 7 are factors, the number itself is returned as a string.
 func Convert(num int) string {
-	factors := FindFactors(num)
+	if num < 3 {
+		return strconv.Itoa(num)
+	}
+	max := num
+	if num > 7 {
+		max = 7
+	}
 	var output string
-	for i := 0; i < len(factors); i++ {
-		if factors[i] == 3 {
-			output = output + "Pling"
-		} else if factors[i] == 5 {
-			output = output + "Plang"
-		} else if factors[i] == 7 {
-			output = output + "Plong"
+	for i := 3; i <= max; i = i + 2 {
+		if num%i == 0 {
+			if i == 3 {
+				output += three
+			}
+			if i == 5 {
+				output += five
+			}
+			if i == 7 {
+				output += seven
+			}
 		}
 	}
 	if output == "" {
