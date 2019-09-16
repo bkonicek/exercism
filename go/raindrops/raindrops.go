79ca44e4e20cdbf75c7ced0a585b93e01ce7e7a5
Ben Konicek <ben.konicek@gmail.com>
2019-09-16 14:10:41 +0000

    modified solution with mentor's comments

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -2,10 +2,6 @@ package raindrops
 
 import "strconv"
 
-const three = "Pling"
-const five = "Plang"
-const seven = "Plong"
-
 // Convert takes an integer and returns 'Pling' if it has 3 as a factor,
 // 'Plang' if it has 5 as a factor, and 'Plong' if it has 7 as a factor.
 // If more than 1 of the above factors apply, their respective strings are all returned.
@@ -14,23 +10,15 @@ func Convert(num int) string {
 	if num < 3 {
 		return strconv.Itoa(num)
 	}
-	max := num
-	if num > 7 {
-		max = 7
-	}
 	var output string
-	for i := 3; i <= max; i = i + 2 {
-		if num%i == 0 {
-			if i == 3 {
-				output += three
-			}
-			if i == 5 {
-				output += five
-			}
-			if i == 7 {
-				output += seven
-			}
-		}
+	if num%3 == 0 {
+		output += "Pling"
+	}
+	if num%5 == 0 {
+		output += "Plang"
+	}
+	if num%7 == 0 {
+		output += "Plong"
 	}
 	if output == "" {
 		return strconv.Itoa(num)
